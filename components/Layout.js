2282b65e9d15e66fffa160465228bc7c4704f02b
Imed Eddine SAKRAOUI <sie-92@users.noreply.github.com>
2025-06-17 00:42:24 +0100

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -3,13 +3,13 @@ import Link from 'next/link';
 
 export default function Layout({ children }) {
   return (
-    <div className="container">
+    <div className="max-w-3xl mx-auto px-4 py-6">
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <header>
+      <header className="mb-8">
         <nav>
-          <Link href="/">🏠 Home</Link>
+          <Link href="/" className="text-blue-600 font-bold text-xl">🏠 Home</Link>
         </nav>
       </header>
       <main>{children}</main>
