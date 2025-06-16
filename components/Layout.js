4c7b6c4dc6386e0a6e6342f56355c1bc63de86a3
Imed Eddine SAKRAOUI <sie-92@users.noreply.github.com>
2025-06-17 00:38:38 +0100

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,17 @@
+import Head from 'next/head';
+import Link from 'next/link';
+
 export default function Layout({ children }) {
   return (
-    <div className="max-w-2xl mx-auto px-4 py-8">
+    <div className="container">
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <header>
+        <nav>
+          <Link href="/">🏠 Home</Link>
+        </nav>
+      </header>
       <main>{children}</main>
     </div>
   );
