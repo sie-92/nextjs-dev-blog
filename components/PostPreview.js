a4c312ab738d3621c0c5b656b1fd4d8157684569
Imed Eddine SAKRAOUI <sie-92@users.noreply.github.com>
2025-06-17 00:42:39 +0100

    Update PostPreview.js

--- components/PostPreview.js
+++ components/PostPreview.js
@@ -2,11 +2,11 @@ import Link from 'next/link';
 
 export default function PostPreview({ id, title, date }) {
   return (
-    <div className="post-preview">
-      <h2>
+    <div className="mb-6 p-4 bg-white rounded shadow hover:shadow-md transition">
+      <h2 className="text-2xl font-semibold text-blue-600">
         <Link href={`/posts/${id}`}>{title}</Link>
       </h2>
-      <small>{date}</small>
+      <p className="text-sm text-gray-500">{date}</p>
     </div>
   );
 }
